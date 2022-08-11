65ffbc3136d1f3d14e4025a28359df84b7c8b650
Kim <99856852+KBryant702@users.noreply.github.com>
2022-08-10 20:48:49 -0700

    Multiple feedback display with rating

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,21 +1,11 @@
-import {useState} from 'react'
 
+function FeedbackItem({item}) {
 
-function FeedbackItem() {
-const [rating, setRating] = useState(7)
-const [text, setText] = useState('This is an example of a Feedback Item')
-
-const handleClick = () => {
-    setRating((prev)=> {
-        console.log(prev)
-        return prev + 1
-    })
-}
     return (
     <div className='card'>
-      <div className="num-display">{rating}</div>
-      <div className="text">T{text}</div>
-      <button onClick ={handleClick}>Click</button>
+      <div className="num-display">{item.rating}</div>
+      <div className="text">{item.text}</div>
+    
     </div>
   )
 }
