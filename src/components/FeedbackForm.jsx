7977dcf0e76b21bb724b4de47889aaf76ad4146c
Kim <99856852+KBryant702@users.noreply.github.com>
2022-08-11 16:02:07 -0700

    Added Rating Select Component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
 import Card from "./shared/Card"
 import Button from "./shared/Button"
+import RatingSelect from "./RatingSelect"
 
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [rating, setRating] = useState(10)
     const [btnDiabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
 
@@ -13,7 +15,7 @@ function FeedbackForm() {
             setBtnDisabled(true)
             setMessage(null)
         }else if(text !== '' && text.trim().length <= 10){
-            setMessage('Text must be at leastr 10 characters')
+            setMessage('Text must be at least 10 characters')
             setBtnDisabled(true)
         }else{
             setMessage(null)
@@ -26,7 +28,7 @@ function FeedbackForm() {
     <Card>
      <form >
         <h2>How would you rate your service with us?</h2>
-        {/* @todo rating select component*/}
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className='input-group'>
           <input
             onChange={handleTextChange}
@@ -37,7 +39,7 @@ function FeedbackForm() {
             <Button type='submit' isDisabled={btnDiabled}> Send</Button>
         </div>
 
-        {message && <div classNAme='message'>{message}</div>}
+        {message && <div className='message'>{message}</div>}
      </form>
     </Card>
   )
